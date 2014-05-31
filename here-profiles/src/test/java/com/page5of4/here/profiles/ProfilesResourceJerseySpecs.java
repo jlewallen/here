dc2e682a3950ca24c6fd31af8c3bebad82ff416c
Jacob Lewallen <jlewallen@gmail.com>
2014-05-30 17:03:27 -0700

    Switch to dropwizard-testing

--- here-profiles/src/test/java/com/page5of4/here/profiles/ProfilesResourceJerseySpecs.java
+++ here-profiles/src/test/java/com/page5of4/here/profiles/ProfilesResourceJerseySpecs.java
@@ -1,5 +1,23 @@
 package com.page5of4.here.profiles;
 
-public class ProfilesResourceJerseySpecs {
+import com.page5of4.here.profiles.tests.ProfilesSpecsModule;
+import com.sun.jersey.api.client.GenericType;
+import io.dropwizard.testing.junit.ResourceTestRule;
+import org.junit.ClassRule;
+import org.junit.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfilesResourceJerseySpecs {
+   @ClassRule
+   public static final ResourceTestRule resources = ResourceTestRule.builder().addResource(new ProfilesSpecsModule().profilesResource()).build();
+
+   @Test
+   public void test_get_profiles_when_empty() {
+      List<Profile> profiles = resources.client().resource("/profiles").get(new GenericType<List<Profile>>() {
+      });
+      assertThat(profiles).isEmpty();
+   }
 }
